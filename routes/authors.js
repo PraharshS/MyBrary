028c88dcd1067c485acc6bba155bf8297eee0afd
Praharsh Singh <praharsh535@gmail.com>
2020-10-25 21:17:59 +0530

    initial author routes

---
+++ routes/authors.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const author = require("../models/author");
+const router = express.Router();
+const Author = require("../models/author");
+// All authors Route
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name !== null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
+  }
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", { authors, searchOptions: req.query });
+  } catch (error) {
+    res.redirect("/");
+  }
+});
+
+// New Author Route
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+
+// Create Author Route
+router.post("/", async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+  try {
+    const newAuthor = await author.save();
+    //   res.redirect(`authors/${newAuthor.id}`);
+    res.redirect("authors");
+  } catch (error) {
+    let locals = { errorMessage: "Error creating Author" };
+    res.render("authors/new", {
+      author: author,
+      locals,
+    });
+  }
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -4,23 +4,31 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connect to Mongoose"));
+const db =
+  "mongodb+srv://draken:yRi6Vlba5JJzGkCD@cluster0.g5z8k.mongodb.net/MyBrary?retryWrites=true&w=majority";
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
